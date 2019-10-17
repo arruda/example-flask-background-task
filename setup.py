5aca6cb0b7ce150ed5646cf323431c7428c63d71
Annanda Sousa <annanda.sousa@insight-centre.org>
2019-10-17 12:27:27 +0100

    making example a python package to be installed in the docker.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='example',
+    version='0.1',
+    author='Annanda Sousa',
+    install_requires=[],
+    packages=['example'],
+    zip_safe=False
+)
