f7f8233157ad1163c75acc6e7a6b4306324b4e35
Annanda Sousa <annanda.sousa@insight-centre.org>
2019-10-21 08:33:58 +0100

    Making the front-end get and give an answer to the user.

--- example/static/example.js
+++ example/static/example.js
@@ -1,52 +1,51 @@
 $(document).ready(function () {
+    var timer = null;
 
-    function print_again() {
-        $("#result").append("<p>Not ready!</p>");
+    function show_result(result_task_id) {
+        $.ajax({
+            url: '/show_result/' + result_task_id,
+            type: 'GET',
+            success: function (msg) {
+                response_task = msg;
+
+                document.getElementById("test_result").innerHTML = response_task.status;
+                if (response_task.status == 'SUCCESS') {
+                    clearInterval(timer);
+                    document.getElementById("result").innerHTML = "The answer is " + response_task.content;
+                    return;
+                }
+            },
+        });
     }
 
-    function call_answer(result) {
-        // document.getElementById("result").innerHTML = result.status
+    function call_answer(result_id) {
         // $("#result").append( "<p>Test</p>" );
-        var count = 0;
+        // document.getElementById("result").innerHTML = 'Not ready, please wait ' + count;
 
-        while (count < 3) {
-            setTimeout(function () {
 
-                // if (result.status != 'SUCCESS') {
-                    // document.getElementById("result").innerHTML = 'Not finished yet';
-                    // setTimeout('print_again()', 1);
-                    // $("#result").append( "<p>Please wait!</p>" )
-                    $("#result").append("<p>Not ready!</p>")
-                // }
-                count++;
+        timer = setInterval(function () {
+                show_result(result_id);
+            },
+            1000)
 
-            }, 3000);
-        }
-
-        // $.ajax({
-        //     url: '/show_result/'+ task_id,
-        //     type: 'GET',
-        //
-        //     }
-        //
-        // )
     }
+
 
     function make_request(text, method = "post") {
         $.ajax({
             url: '/call_task',
             type: 'POST',
-            // data: {
-            //     email: 'email@example.com',
-            //     message: text
-            // },
             success: function (msg) {
                 response_task = msg;
-                call_answer(response_task);
+                call_answer(response_task.result_task_id);
+                // document.getElementById("result").innerHTML = "wait for a while!";
+
             },
             // async: false,
         });
-        // document.getElementById("result").innerHTML = "wait for a while!"
+        // )
+        // document.getElementById("result").innerHTML = "wait for a while!";
+        // call_answer('done');
 
         // document.getElementById("result").innerHTML = prediction
         // change_prediction_result(prediction);
